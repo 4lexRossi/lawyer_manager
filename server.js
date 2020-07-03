fbd96a43a52b344d0a7ae633bccc76e1797e9b1f
4lexRossi <devalexrossi@gmail.com>
2020-07-03 20:05:45 -0300

    deploy

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/Dio-processUI'));
+app.use(express.static(__dirname + '/dist/lawyer_manager'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/Dio-processUI/index.html'));
+res.sendFile(path.join(__dirname+'/dist/lawyer_manager/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
